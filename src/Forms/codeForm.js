87ba0fcabcc16c96f600d384b2a662acb68e7935
DamirMinibaev <damir.minib2014@gmail.com>
2021-05-18 18:10:46 +0300

    Feat: Added validation on betaPage

--- src/Forms/codeForm.js
+++ src/Forms/codeForm.js
@@ -2,16 +2,22 @@ import {Field, reduxForm} from "redux-form";
 import React from "react";
 
 const secretForm = (props) => {
+    const {error, submitting} = props
     return (
         <form onSubmit={props.handleSubmit}>
             <div className="input-group mb-3">
                 <Field placeholder={"Secret code"} name={"secretCode"}
-                       component="Input" type = "password" className="form-control" />
-                <button className="btn btn-outline-secondary button">Send code</button>
+                       component="input" type="password" className={error ? "form-control is-invalid" : "form-control"}
+                       required/>
+                <button className="btn btn-outline-secondary button_form" disabled={submitting}>Send code</button>
+                <div className={error ? "invalid-tooltip" : "hidden"} >
+                    {error}
+                </div>
             </div>
         </form>
     )
 }
+
 const SecretReduxForm = reduxForm({
     form: 'secret'
 })(secretForm)
